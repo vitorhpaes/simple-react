86b68424e80ad8b7425c6c4e4ef4c5edf0d9f15f
Vítor Paes <vitor.paes@compasso.com.br>
2024-02-23 20:06:19 -0300

    fix: create .env extractor to exemplify

--- src/services/giphy/request.ts
+++ src/services/giphy/request.ts
@@ -1,6 +1,10 @@
 import axios from 'axios'
 import { GIPHY_API_KEY, GIPHY_API_URL } from '../../constants/API_CONFIG'
 
+// or
+// const GIPHY_API_URL = import.meta.env.VITE_GIPHY_API_URL
+// const GIPHY_API_KEY = import.meta.env.VITE_GIPHY_API_KEY
+
 const giphyRequest = axios.create({
     baseURL: GIPHY_API_URL,
     params: {
